1bbdd4ad0264361b960012104c8dc3cb4d3ea586
deris <305945915@qq.com>
2018-01-21 16:14:24 +0800

    move font library && update README

--- example/App.vue
+++ example/App.vue
@@ -14,6 +14,6 @@ export default {
 </script>
 
 <style lang="scss">
-  @import "assets/css/fontawesome.css";
+  @import "../src/vendor/css/fontawesome.css";
   @import "../src/styles/index.scss";
 </style>
