195ecbeeb805ddea087437b3ac356f9c5151a709
deris <305945915@qq.com>
2018-01-29 19:25:11 +0800

    add emitter

---
+++ src/mixins/emitter.js
@@ -0,0 +1,33 @@
+const _broadcast = (componentName, eventName, args) => {
+  this.$children.forEach(function (child) {
+    let name = _getComponentName(child)
+
+    if (name === componentName) {
+      child.$emit.apply(child, [eventName, args])
+    } else {
+      _broadcast.apply(child, [componentName, eventName, args])
+    }
+  })
+}
+
+const _getComponentName = el => el.$options.name
+
+export default {
+  methods: {
+    dispatch (componentName, eventName, args) {
+      let parent = this.$parent || this.$root
+      let name = _getComponentName(parent)
+
+      while (parent && (!name || name !== componentName)) {
+        parent = parent.$parent
+        parent && (name = _getComponentName(parent))
+      }
+
+      parent.$emit.apply(parent, [eventName, args])
+    },
+
+    broadcast (componentName, eventName, args) {
+      _broadcast.apply(this, [componentName, eventName, args])
+    }
+  }
+}
