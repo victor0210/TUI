91c1c2b2865f560b52c87e6195abe00d14cea566
deris <305945915@qq.com>
2018-02-03 18:22:13 +0800

    component: add normal select

---
+++ src/components/option/option.vue
@@ -0,0 +1,21 @@
+<template>
+  <li>{{ label }}</li>
+</template>
+
+<script>
+export default {
+  name: 't-option',
+  data () {
+  },
+  props: {
+    label: String
+  },
+
+  methods: {}
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/select/select.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+  export default {
+    name: "select"
+  }
+</script>
+
+<style scoped>
+
+</style>
