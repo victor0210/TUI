65858ace756aa65d04520ecb6a0d6aacb8934be6
deris <305945915@qq.com>
2018-03-25 00:17:41 +0800

    remove select bak

--- src/components/cascader-bak/index.js
+++
@@ -1,9 +0,0 @@
-import Cascader from './cascader.vue'
-import Vue from 'vue'
-
-Cascader.install = function () {
-  if (this.install.installed) return
-  Vue.component(Cascader.name, Cascader)
-}
-
-export default Cascader
--- src/components/select-bak/index.js
+++
@@ -1,9 +0,0 @@
-import Select from './select.vue'
-import Vue from 'vue'
-
-Select.install = function () {
-  if (this.install.installed) return
-  Vue.component(Select.name, Select)
-}
-
-export default Select
