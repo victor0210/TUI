94fffc5a286db09808f11faa76f6312129346ca2
deris <305945915@qq.com>
2018-04-01 21:28:11 +0800

    complete check data flow

--- src/components/tree/tree-item.vue
+++ src/components/tree/tree-item.vue
@@ -50,6 +50,7 @@ export default {
       isChecked: false,
       childrenCheckCount: 0,
       indeterminateChildren: 0,
+      reechoCount: 0,
       indeterminate: false,
       loaded: false,
       loading: false
@@ -77,30 +78,58 @@ export default {
   },
 
   mounted () {
-    this.isChecked = this.initChecked
+    this.initChecked && this.checkChangeHandler(this.initChecked, true)
     this.initExpand && (this.expand = true)
   },
 
   methods: {
-    echoHandler () {},
-    reechoHandler () {},
-    checkChangeHandler (val) {
-      !this.checkDisabled && (this.isChecked = val)
-      this.broadcast('t-tree-item', 'check-change', val)
+    echoHandler (val) {
+      if (this.hasChildren) {
+        this.broadcast('t-tree-item', 'node-item-echo', val)
+      } else {
+        !this.checkDisabled && (this.isChecked = val)
+        this.TTreeItem && this.dispatch('t-tree-item', 'node-item-reecho', val)
+      }
+    },
+    reechoHandler () {
+      this.isChecked = this.childCheckAll()[0]
+      this.indeterminate = this.childCheckAll()[1]
+      this.TTreeItem && this.dispatch('t-tree-item', 'node-item-reecho', this.isChecked)
+    },
+    checkChangeHandler (val, init = false) {
+      if (!init && this.checkDisabled) return
+
+      if (this.hasChildren) {
+        this.broadcast('t-tree-item', 'node-item-echo', val)
+      } else {
+        this.isChecked = val
+        this.TTreeItem && this.dispatch('t-tree-item', 'node-item-reecho', val)
+
+        this.dispatch('t-tree', 'check-change', {
+          label: this.label,
+          checked: val,
+          indeterminate: this.indeterminate
+        })
+      }
     },
     checkout () {
       this.expand = !this.expand
+    },
+    childCheckAll () {
+      let checkCount = 0
+      let indeterminateCount = 0
+      this.$children.forEach(function (el, idx) {
+        if (idx > 0) {
+          el.isChecked && checkCount++
+          el.indeterminate && indeterminateCount++
+        }
+      })
+
+      return [checkCount === this.childLen, (checkCount > 0 && checkCount < this.childLen) || indeterminateCount > 0]
     }
   },
 
   watch: {
-    isChecked (val) {
-      this.dispatch('t-tree', 'check-change', {
-        label: this.label,
-        checked: val,
-        indeterminate: this.indeterminate
-      })
-    },
     expand (val) {
       this.dispatch('t-tree', 'node-click', {
         label: this.label
@@ -109,6 +138,11 @@ export default {
         this.loading = true
         this.dispatch('t-tree', 'lazy-load', this)
       }
+    }
+  },
+  computed: {
+    childLen () {
+      return this.$children.length - 1
     }
   }
 }
