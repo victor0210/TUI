057f53f7c2cd7dd9fba860af0cb1e530c7eb7cca
deris <305945915@qq.com>
2018-04-07 15:10:46 +0800

    docs: complete notify

--- example/pages/notify.vue
+++ example/pages/notify.vue
@@ -28,13 +28,51 @@
       </div>
       <radio-group/>
     </div>
+
+    <div class="demo-block">
+      <div class="subtitle">带状态</div>
+      <div class="source">
+        <t-row :gutter="20">
+          <t-col :span="24">
+            <t-button type="primary" outline @click="success(c1)">success</t-button>
+            <t-button type="primary" outline @click="info(c1)">info</t-button>
+            <t-button type="primary" outline @click="warning(c1)">warning</t-button>
+            <t-button type="primary" outline @click="danger(c1)">danger</t-button>
+          </t-col>
+        </t-row>
+      </div>
+      <radio-group/>
+    </div>
+
+    <div class="demo-block">
+      <div class="subtitle">详细信息</div>
+      <div class="source">
+        <t-row :gutter="20">
+          <t-col :span="24">
+            <t-button type="primary" outline @click="success(c7)">详细信息</t-button>
+          </t-col>
+        </t-row>
+      </div>
+      <radio-group/>
+    </div>
+
+    <div class="demo-block">
+      <div class="subtitle">隐藏关闭按钮</div>
+      <div class="source">
+        <t-row :gutter="20">
+          <t-col :span="24">
+            <t-button type="primary" outline @click="success(c8)">隐藏关闭按钮</t-button>
+          </t-col>
+        </t-row>
+      </div>
+      <radio-group/>
+    </div>
   </div>
 </template>
 
 <script>
 import RadioGroup from '../documents/radio/radioGroup.md'
-// duration: 0,
-//   sub: '系统将于2018年8月8日晚 00:00 - 02:00 进行维护，届时将不能登录，很抱歉给您造成不便!'
+
 export default {
   components: {
     RadioGroup
@@ -64,6 +102,15 @@ export default {
       c6: {
         title: '维护通知',
         duration: 0
+      },
+      c7: {
+        title: '维护通知',
+        sub: '系统将于2018年8月8日晚 00:00 - 02:00 进行维护，届时将不能登录，很抱歉给您造成不便!'
+      },
+      c8: {
+        closable: false,
+        title: '维护通知',
+        sub: '系统将于2018年8月8日晚 00:00 - 02:00 进行维护，届时将不能登录，很抱歉给您造成不便!'
       }
     }
   },
@@ -71,6 +118,18 @@ export default {
   methods: {
     open (opt) {
       this.$TNotify.show(opt)
+    },
+    success (opt) {
+      this.$TNotify.success(opt)
+    },
+    info (opt) {
+      this.$TNotify.info(opt)
+    },
+    warning (opt) {
+      this.$TNotify.warning(opt)
+    },
+    danger (opt) {
+      this.$TNotify.danger(opt)
     }
   }
 }
