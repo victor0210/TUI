a69570cdb24806abd7152f1765b6b64d506b13df
deris <305945915@qq.com>
2018-05-03 14:06:59 +0800

    feat: website header style

--- example/components/Header.vue
+++ example/components/Header.vue
@@ -1,35 +1,30 @@
 <template>
     <t-row>
-        <t-col :span="10" class="logoCol">
+        <t-col :span="6" class="logoCol">
           <a href="/#"><img src="../assets/TV.jpg" alt="" class="logo-img"></a>
           <span style="font-size: 20px; color: #606060; font-family: 'Apple Chancery'">Think-UI</span>
         </t-col>
-        <t-col :span="5">
+      <t-col :span="8" class="navCol">
+        <ul>
+          <li>
+            <a href="#">
+              <span>指南</span>
+            </a>
+          </li>
+          <li>
+            <router-link to="/components" class="header-link">组件</router-link>
+          </li>
+          <li>
+            <a href="#">
+              <span>教程</span>
+            </a>
+          </li>
+        </ul>
+      </t-col>
+        <t-col :span="10">
             <div class="search">
-                <t-input placeholder="搜索组件....">
-                    <t-button slot="suffix" type="primary" icon="fa fa-search"></t-button>
-                </t-input>
+                <t-input placeholder="搜索组件" prefix-icon="fa fa-search"></t-input>
             </div>
-        </t-col>
-        <t-col :span="8" class="navCol">
-            <ul>
-                <li>
-                    <a href="#">
-                        <span>指南</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <span>组件</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <span>教程</span>
-                    </a>
-                </li>
-            </ul>
-
         </t-col>
     </t-row>
 </template>
@@ -37,6 +32,6 @@
 export default {
 }
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
     @import "../styles/header";
 </style>
